59951031c70791a86f083de8b36b6328f7acf263
Timmy Yellow dick <tiyellow@e2r7p6.21-school.ru>
2018-12-02 06:27:54 +0300

    1 part ready

---
+++ ft_strlcat.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tiyellow <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/12/02 04:16:21 by tiyellow          #+#    #+#             */
+/*   Updated: 2018/12/02 06:14:22 by tiyellow         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
+{
+	size_t i;
+	size_t dlen;
+	size_t slen;
+
+	i = 0;
+	dlen = ft_strlen(dst);
+	slen = ft_strlen(src);
+	if (size <= dlen)
+		return (slen + size);
+	i = dlen;
+	while (*src && i < size - 1)
+	{
+		dst[i] = *src;
+		i++;
+		src++;
+	}
+	dst[i] = '\0';
+	return (dlen + slen);
+}
--- libft.h
+++ libft.h
@@ -26,6 +26,7 @@ void    *ft_memcpy(void *dst, const void *src, size_t n);
 void    *ft_memmove(void *dst, const void *src, size_t len);
 void    *ft_memset(void *b, int c, size_t len);
 char    *ft_strcat(char *s1, const char *s2);
+size_t  ft_strlcat(char *dst, const char *src, size_t size);
 char    *ft_strchr(const char *s, int c);
 int        ft_strcmp(const char *s1, const char *s2);
 char    *ft_strcpy(char *dst, const char *src);
